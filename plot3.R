6f70e930899791ae11510894b10d7d8f53cada33
Emilie Wolf <emilie.h.wolf@gmail.com>
2017-05-07 03:48:03 -0500

    add code to plot3.R script and upload plot3.png

--- plot3.R
+++ plot3.R
@@ -7,4 +7,19 @@ if(!exists("pow")) {
         source("readplotdata.R")
 }
 
-## Plot...
+## Open PNG graphic device
+png(filename = "plot3.png",width=480,height=480)
+
+## Plot the line chart to the PNG file
+with(pow, plot(datetime,Sub_metering_1, type="n", 
+               bg="transparent", xlab='', 
+               ylab='Energy sub metering'))
+lines(pow$datetime,pow$Sub_metering_1)
+lines(pow$datetime,pow$Sub_metering_2,col="red")
+lines(pow$datetime,pow$Sub_metering_3,col="blue")
+legend("topright",legend=names(pow)[7:9],
+       col=c("black","red","blue"),
+       lwd=2)
+
+## Close and save PNG graphic
+dev.off()
